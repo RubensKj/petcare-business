6704e07b21703c2a2125185e89d259566213a3eb
RubensKj <rubenskleinschmidtjr@gmail.com>
2019-08-08 22:11:07 -0300

    Login Page

---
+++ src/Pages/LogIn/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import HeaderBoxAuth from '../../Components/HeaderBoxAuth';
+
+import './styles.css';
+
+export default function LogIn() {
+  return (
+    <div className="container-login">
+      <div className="box-login">
+        <HeaderBoxAuth message="Entre no PetCare" />
+        <form className="login-form" onSubmit={null} autocomplete="off">
+          <div className="error-area">
+            <h3 className="error-login">Errooooooooooooooooooooooooooooooooooo</h3>
+          </div>
+          <div className="input-area">
+            <label>Email:</label>
+            <div className="input-div">
+              <input type="text" name="email" onChange={null} />
+            </div>
+          </div>
+          <div className="input-area">
+            <label>Senha:</label>
+            <div className="input-div">
+              <input type="password" name="password" onChange={null} />
+            </div>
+          </div>
+          <div className="button-area">
+            <button type="submit">Entrar</button>
+          </div>
+        </form>
+      </div>
+      <div className="box-ref-signup">
+        <div className="content-signup-box">
+          <div className="header-ref-signup">
+            <span>Não possui cadastro?</span>
+          </div>
+          <div className="button-signup-area">
+            <a href="/cadastrar">Cadastre-se</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -6,13 +6,16 @@ import CreateService from './Pages/CreateService';
 import EditPage from './Pages/EditPage';
 import ListProducts from './Pages/ListProducts';
 import Preview from './Pages/Preview';
+
 import SignUp from './Pages/SignUp';
+import LogIn from './Pages/LogIn';
 
 const Routes = () => (
   <BrowserRouter>
       <Switch>
           <Route exact path='/' component={Preview} />
           <Route path='/cadastrar' component={SignUp} />
+          <Route path='/entrar' component={LogIn} />
           <Route path='/cadastrar-servico' component={CreateService} />
           <Route path='/lista-produtos' component={ListProducts} />
           <Route path='/produtos/id/editar' component={EditPage} />
