983f298f6774d6f2fe1e967a16e90cbf7c17b1ee
RubensKj <rubenskleinschmidtjr@gmail.com>
2019-08-15 21:53:51 -0300

    Fixing classNames tags

--- src/Components/TransitionOfSetting/index.js
+++ src/Components/TransitionOfSetting/index.js
@@ -4,8 +4,8 @@ import './styles.css';
 
 export default function TransitionOfSetting({ title, description }) {
   return (
-    <div className="info-header">
-      <div className="content-header">
+    <div className="info-header-settings">
+      <div className="content-header-settings">
         <h3>{title}</h3>
         <span>{description}</span>
       </div>
