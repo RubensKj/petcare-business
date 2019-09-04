4651a13b491cca84f683f1ce67f62db07e7f577d
RubensKj <rubenskleinschmidtjr@gmail.com>
2019-09-04 16:25:37 -0300

    Fixing signup (somethings)

--- src/Pages/SignUpOwner/index.js
+++ src/Pages/SignUpOwner/index.js
@@ -42,7 +42,33 @@ export default function SignUpOwner(props) {
         return;
       }
 
-      if (cpf < 0 || cpf.length > 14 || cpf.length < 11) {
+      if (cpf.length <= 9 || cpf.length >= 12) {
+        dispatch(addErrors("Este CPF é inválido, favor inserir um válido"));
+        addAnimationToInput();
+        return;
+      }
+
+      var Soma;
+      var Resto;
+      Soma = 0;
+      if (cpf === "00000000000") {
+        dispatch(addErrors("Este CPF é inválido, favor inserir um válido"));
+        addAnimationToInput();
+        return;
+      }
+
+      for (var i = 1; i <= 9; i++) Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+      Resto = (Soma * 10) % 11;
+
+      if ((Resto === 10) || (Resto === 11)) Resto = 0;
+      if (Resto !== parseInt(cpf.substring(9, 10))) return false;
+
+      Soma = 0;
+      for (i = 1; i <= 10; i++) Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+      Resto = (Soma * 10) % 11;
+
+      if ((Resto === 10) || (Resto === 11)) Resto = 0;
+      if (Resto !== parseInt(cpf.substring(10, 11))) {
         dispatch(addErrors("Este CPF é inválido, favor inserir um válido"));
         addAnimationToInput();
         return;
