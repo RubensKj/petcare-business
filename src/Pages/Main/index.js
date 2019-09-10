311af7adef3cc8b0d74edc37bf191cf66b9743ec
RubensKj <rubenskleinschmidtjr@gmail.com>
2019-09-10 15:52:42 -0300

    Fixing responsive

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -3,6 +3,9 @@ import React, { useEffect } from 'react';
 import HeaderMainPage from '../../Components/HeaderMainPage';
 import Footer from '../../Components/Footer';
 
+import dog from "../../Assets/dog-main.png";
+import cat from "../../Assets/cat-main.jpg";
+
 import { isAuthenticated } from '../../Services/auth';
 
 import './styles.css';
@@ -10,7 +13,7 @@ import './styles.css';
 export default function Main(props) {
 
   useEffect(() => {
-    if(isAuthenticated()) {
+    if (isAuthenticated()) {
       props.history.push('/dashboard')
     }
   }, [props.history])
@@ -18,8 +21,31 @@ export default function Main(props) {
   return (
     <>
       <HeaderMainPage />
-      <div className="container-content">
+      <div className="body-main">
+        <div className="container-main">
+          <div className="banner-main-dog">
+            <img src={dog} alt="" />
 
+            <div className="banner-h1">
+              <h1>Em prol do bem-estar do seu pet!</h1>
+            </div>
+
+            <div className="banner-p">
+              <p>A felicidade do seu amigo está a um clique</p>
+              <a href="/entrar">Crie sua conta</a>
+            </div>
+          </div>
+          <div className="banner-main-cat">
+            <img src={cat} alt="" />
+            <div className="banner-h1-cat">
+              <h1>Venha fazer a diferença!</h1>
+            </div>
+            <div className="banner-p-cat">
+              <p>Faça parte da família PetCare</p>
+            </div>
+            <a href="/cadastrar">Cadastre sua empresa</a>
+          </div>
+        </div>
       </div>
       <Footer />
     </>
